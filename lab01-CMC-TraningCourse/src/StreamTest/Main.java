c76e2df1cb59a895eec3d8c47cfd9099dd65938b
vtnghia1 <vtnghia1@cmcglobal.vn>
2022-08-03 17:22:02 +0700

    commit lab2

--- lab01-CMC-TraningCourse/src/StreamTest/Main.java
+++ lab01-CMC-TraningCourse/src/StreamTest/Main.java
@@ -1,11 +1,9 @@
 package StreamTest;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
